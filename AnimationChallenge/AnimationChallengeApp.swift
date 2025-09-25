53dc7aacaf4a99fe832d6a1e1e3e91cb3ea66bc2
Mac <macjacobson99@gmail.com>
2025-09-25 13:25:54 -0600

    Initial Commit

---
+++ AnimationChallenge/AnimationChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationChallengeApp.swift
+//  AnimationChallenge
+//
+//  Created by Jacobson, Machiah - Student on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationChallengeTests/AnimationChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationChallengeTests.swift
+//  AnimationChallengeTests
+//
+//  Created by Jacobson, Machiah - Student on 9/25/25.
+//
+
+import Testing
+@testable import AnimationChallenge
+
+struct AnimationChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
